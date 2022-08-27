bf9221a5aff34131dfab4c9794e02a8035322104
shiyi.chen <dachong_313@163.com>
2022-08-27 21:18:38 +0800

    push commit

--- src/main/java/work/loopgain/git/GitTest.java
+++ src/main/java/work/loopgain/git/GitTest.java
@@ -9,5 +9,6 @@ public class GitTest {
         System.out.println("Hello Git3");
         System.out.println("hot-fix test");
         System.out.println("master test");
+        System.out.println("push test");
     }
 }
