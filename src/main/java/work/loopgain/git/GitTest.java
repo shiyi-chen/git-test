58f1aa97b8f842fd45f06f1ba2e308e474e7b819
shiyi.chen <dachong_313@163.com>
2022-08-27 23:59:26 +0800

    gitee commit2

--- src/main/java/work/loopgain/git/GitTest.java
+++ src/main/java/work/loopgain/git/GitTest.java
@@ -13,5 +13,7 @@ public class GitTest {
         System.out.println("pull test");
         System.out.println("gitee test1");
         System.out.println("gitee test2");
+        System.out.println("gitee test2");
+        System.out.println("gitee test4");
     }
 }
