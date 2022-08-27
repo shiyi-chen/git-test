26790200f60ce1c41937e90b24596c9d72579997
shiyi.chen <dachong_313@163.com>
2022-08-27 19:34:37 +0800

    second commit

--- src/main/java/work/loopgain/git/GitTest.java
+++ src/main/java/work/loopgain/git/GitTest.java
@@ -3,6 +3,7 @@ package work.loopgain.git;
 public class GitTest {
 
     public static void main(String[] args) {
-        System.out.println("Hello Git");
+        System.out.println("Hello Git1");
+        System.out.println("Hello Git2");
     }
 }
